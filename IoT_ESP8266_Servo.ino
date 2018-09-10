fe42beb5988b42a74760f6c308cd66c543ecffc6
mikroBass <43114339+mikroBass@users.noreply.github.com>
2018-09-10 16:11:24 +0200

    Update IoT_ESP8266_Servo.ino

--- IoT_ESP8266_Servo.ino
+++ IoT_ESP8266_Servo.ino
@@ -1,14 +1,17 @@
 /*
-  MOOC Fabriquer un objet connecté : TP semaine 3
+  MOOC IMT Fabriquer un objet connecté : TP semaine 3
 
- Implémentation d'un Nelson Interrupteur
+ Implémentation d'un capteur angulaire
 
  Lit la valeur analogique du servo moteur sur la broche et envoie
- cette valeur sur la plateforme TOM en utilisant une requête PATCH
- Toutes les 3 secondes, nous récupérons la valeur sur TOM afin de voir si le
- servo a bougé
+ cette valeur sur la plateforme TOM 
+ (serveur HTTP mis en place par l'IMT pour le Mooc - utilisation de requête PATCH )
+
+ Toutes les 3 secondes, la valeur est transmise à TOM afin de voir si le servo a bougé
 
  Réalisation de l'exercice à partir du squelette de code fourni par l'équipe de l'IMT
+ 
+ Ce code est sous licence CC0.1
 
  François Jarriges 2017
 
